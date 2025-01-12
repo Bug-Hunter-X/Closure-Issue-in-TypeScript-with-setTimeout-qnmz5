26c75230b1fe9cf3e07e91e649a51599135e6822
BugHunter-X <askzenai@gmail.com>
2025-01-12 09:48:05 +0100

    Add bugSolution.ts file

---
+++ bugSolution.ts
@@ -0,0 +1,19 @@
+function printNumbers(n: number): void {
+  for (let i = 1; i <= n; i++) {
+    console.log(i);
+  }
+}
+
+printNumbers(5); // This will work fine
+
+function printNumbers2(n: number): void {
+  for (let i = 1; i <= n; i++) {
+    (function(j) {
+      setTimeout(() => {
+        console.log(j);
+      }, 1000);
+    })(i);
+  }
+}
+
+printNumbers2(5); // This will correctly print 1 to 5 with delays
